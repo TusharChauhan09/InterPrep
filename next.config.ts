3d36cf232296d0800446e38a0d226e0c29aad4f4
Tushar Chauhan <tusharchauhan0912@gmail.com>
2026-03-08 18:19:16 +0530

    Add output standalone to fix nested route groups build issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   // for clerk images
   images: {
     domains: ["img.clerk.com"], // Add Clerk's image host here
